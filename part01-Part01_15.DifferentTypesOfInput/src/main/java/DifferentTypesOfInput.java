0c77b957ea9df3d15d3450eda5ee91f1497a729c
deprgym93 <giorgos9cy@gmail.com>
2022-05-01 19:08:14 +0300

    initial commit

---
+++ part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class DifferentTypesOfInput {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give a string:");
+        String input = scan.nextLine();
+        System.out.println("Give an integer:");
+        int num = Integer.valueOf(scan.nextLine());
+        System.out.println("Give a double:");
+        double num2 = Double.valueOf(scan.nextLine());
+        System.out.println("Give a boolean:");
+        boolean value = Boolean.valueOf(scan.nextLine());
+        
+        System.out.println("You gave the string " + input);
+        System.out.println("You gave the integer " + num);
+        System.out.println("You gave the double " + num2);
+        System.out.println("You gave the boolean " + value);
+    }
+}
